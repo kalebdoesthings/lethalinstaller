93675c30c7e751cec400a4a75477f45694d8176f
kaleb <94716073+kalebdoesthings@users.noreply.github.com>
2024-01-07 12:56:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ function Install() {
   window.open("ror2mm://v1/install/thunderstore.io/FlipMods/TooManyEmotes/1.6.3/")
   window.open("ror2mm://v1/install/thunderstore.io/FlipMods/ObjectVolumeController/1.0.3/")
   window.open("ror2mm://v1/install/thunderstore.io/Suskitech/AlwaysHearActiveWalkies/1.4.3/")
-  alert("Done! \nYou Should Have 8 Mods")
+  window.open("ror2mm://v1/install/thunderstore.io/RugbugRedfern/Skinwalkers/2.0.1/")
+    alert("Done! \nYou Should Have 9 Mods")
   }
   else {
     alert("Flip You Numbskull!")
